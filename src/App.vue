5a4451b741e4fd74cf3ada6ee2276459c5a06d5a
Danil Popov <zabastx@gmail.com>
2025-07-30 09:48:37 +0300

    app config update

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,7 @@
 <template>
   <h1>You did it!</h1>
   <p>
-    Visit
-
-    <a href="https://vuejs.org/"
-    target="_blank" rel="noopener">
-    vuejs.org</a> to read the
-    documentation
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener"> vuejs.org</a> to read the documentation
   </p>
 </template>
 

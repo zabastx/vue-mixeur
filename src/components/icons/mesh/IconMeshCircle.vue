7d6e686ff81e50a2d07436a8637eaad745a0ca11
Danil Popov <zabastx@gmail.com>
2025-10-16 02:34:13 +0300

    mesh icons

---
+++ src/components/icons/mesh/IconMeshCircle.vue
@@ -0,0 +1,11 @@
+<template>
+	<svg height="1em" viewBox="0 0 1600 1600" width="1em" xmlns="http://www.w3.org/2000/svg">
+		<g fill="#fff">
+			<path
+				d="m55 389c-3.86012 0-7 3.13988-7 7s3.13988 7 7 7 7-3.13988 7-7-3.13988-7-7-7zm0 1c3.31968 0 6 2.68032 6 6s-2.68032 6-6 6-6-2.68032-6-6 2.68032-6 6-6z"
+				fill-rule="evenodd"
+				transform="matrix(100 0 0 100 -4700 -38800)"
+			/>
+		</g>
+	</svg>
+</template>

256ef9f17041aab56896c4f6cca808b5b51f30e8
Danil Popov <zabastx@gmail.com>
2025-10-16 03:34:02 +0300

    fix: app store rework
    - fixed app hotkeys conflict with browser hotkeys
    - setview refactor

--- src/store/app.ts
+++ src/store/app.ts
@@ -8,10 +8,9 @@ export const useAppStore = defineStore('app', () => {
 	const pointerOnCanvas = ref(false)
 	const isCtrlDown = ref(false)
 	const isShiftDown = ref(false)
+	const sceneStore = useThreeStore()
 
 	function useHotKeys(canvas: ShallowRef<HTMLCanvasElement | null>) {
-		const sceneStore = useThreeStore()
-
 		useEventListener(canvas, 'pointerenter', () => (pointerOnCanvas.value = true))
 		useEventListener(canvas, 'pointerleave', () => (pointerOnCanvas.value = false))
 
@@ -32,17 +31,17 @@ export const useAppStore = defineStore('app', () => {
 					sceneStore.switchCamera()
 					break
 
-				case 'Numpad1': // Front view
-					setView(0, 0, 1, e.ctrlKey)
+				case 'Numpad1': // Front / Back view
+					e.preventDefault()
+					setView({ z: 1, invert: e.ctrlKey })
 					break
-				case 'Numpad3': // Right view
-					setView(1, 0, 0, e.ctrlKey)
+				case 'Numpad3': // Right / Left view
+					e.preventDefault()
+					setView({ x: 1, invert: e.ctrlKey })
 					break
-				case 'Numpad7': // Top view
-					setView(0, 1, 0, e.ctrlKey)
-					break
-				case 'Numpad9': // Bottom view
-					setView(0, 1, 0, !e.ctrlKey)
+				case 'Numpad7': // Top / Bottom view
+					e.preventDefault()
+					setView({ y: 1, invert: e.ctrlKey })
 					break
 
 				case 'KeyG':
@@ -84,23 +83,33 @@ export const useAppStore = defineStore('app', () => {
 			}
 			controls.update()
 		})
+	}
 
-		function setView(x: number, y: number, z: number, invert: boolean) {
-			const { activeCamera, controls } = sceneStore
-			if (!controls || !activeCamera) return
-			const dir = invert ? -1 : 1
-			const distance = activeCamera.position.distanceTo(controls.target)
-			const newPos = new THREE.Vector3(x * dir, y * dir, z * dir).multiplyScalar(distance)
-			activeCamera.position.copy(controls.target).add(newPos)
-			activeCamera.up.set(0, 1, 0)
-			activeCamera.lookAt(controls.target)
-			controls.update()
-		}
+	function setView({
+		x = 0,
+		y = 0,
+		z = 0,
+		invert = false
+	}: {
+		x?: number
+		y?: number
+		z?: number
+		invert?: boolean
+	}) {
+		const { activeCamera, controls } = sceneStore
+		if (!controls || !activeCamera) return
+		const dir = invert ? -1 : 1
+		const distance = activeCamera.position.distanceTo(controls.target)
+		const newPos = new THREE.Vector3(x * dir, y * dir, z * dir).multiplyScalar(distance)
+		activeCamera.position.copy(controls.target).add(newPos)
+		activeCamera.up.set(0, 1, 0)
+		activeCamera.lookAt(controls.target)
+		controls.update()
 	}
 
 	const showStatusBar = ref(true)
 
-	return { pointerOnCanvas, isCtrlDown, isShiftDown, useHotKeys, showStatusBar }
+	return { pointerOnCanvas, isCtrlDown, isShiftDown, useHotKeys, showStatusBar, setView }
 })
 
 if (import.meta.hot) {

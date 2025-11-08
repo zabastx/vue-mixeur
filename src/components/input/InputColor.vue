ad036d58bf7204bcd2af198eeb93ed4e8b255e16
Danil Popov <zabastx@gmail.com>
2025-11-08 21:56:49 +0300

    feat(input): add InputColor component for color picker

---
+++ src/components/input/InputColor.vue
@@ -0,0 +1,9 @@
+<template>
+	<input v-model="model" type="color" class="h-full rounded w-[150px]" />
+</template>
+
+<script lang="ts" setup>
+import type { InputHTMLAttributes } from 'vue'
+
+const model = defineModel<InputHTMLAttributes['value']>()
+</script>

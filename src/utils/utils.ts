c4eceffb9bbe6ba4c8abdbae7cbdde9ca8984050
Danil Popov <zabastx@gmail.com>
2026-02-04 22:00:21 +0300

    refactor(utils): improve bytesToSize utility function

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,7 +1,8 @@
 export function bytesToSize(bytes: number) {
-	const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB']
-	if (bytes === 0) return 'n/a'
+	const sizes = ['Bytes', 'KB', 'MB', 'GB']
+	if (bytes <= 0) return 'n/a'
 	const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)).toString(), 10)
 	if (i === 0) return `${bytes} ${sizes[i]})`
+	if (i > 3) return `${(bytes / 1024 ** 3).toFixed(1)} ${sizes[3]}`
 	return `${(bytes / 1024 ** i).toFixed(1)} ${sizes[i]}`
 }

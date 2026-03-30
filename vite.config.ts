fcc2beb2219e84cb181472e3b63b82cbaabd0183
Danil Popov <zabastx@gmail.com>
2026-03-31 00:12:50 +0300

    chore(config): remove Google Fonts and rsms font caching from service worker

--- vite.config.ts
+++ vite.config.ts
@@ -60,48 +60,6 @@ export default defineConfig({
 				globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2,exr}'],
 				runtimeCaching: [
 					{
-						urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
-						handler: 'CacheFirst',
-						options: {
-							cacheName: 'google-fonts-cache',
-							expiration: {
-								maxEntries: 10,
-								maxAgeSeconds: 60 * 60 * 24 * 365
-							},
-							cacheableResponse: {
-								statuses: [0, 200]
-							}
-						}
-					},
-					{
-						urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
-						handler: 'CacheFirst',
-						options: {
-							cacheName: 'gstatic-fonts-cache',
-							expiration: {
-								maxEntries: 10,
-								maxAgeSeconds: 60 * 60 * 24 * 365
-							},
-							cacheableResponse: {
-								statuses: [0, 200]
-							}
-						}
-					},
-					{
-						urlPattern: /^https:\/\/rsms\.me\/.*/i,
-						handler: 'CacheFirst',
-						options: {
-							cacheName: 'rsms-fonts-cache',
-							expiration: {
-								maxEntries: 10,
-								maxAgeSeconds: 60 * 60 * 24 * 365
-							},
-							cacheableResponse: {
-								statuses: [0, 200]
-							}
-						}
-					},
-					{
 						urlPattern: /\.(?:png|jpg|jpeg|svg|gif|webp|exr)$/,
 						handler: 'CacheFirst',
 						options: {

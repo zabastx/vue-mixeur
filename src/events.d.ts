a7a8fb87ef93ba0bddabc6686eaca195ea94085c
Danil Popov <zabastx@gmail.com>
2026-04-15 04:13:54 +0300

    feat(camera): Add dedicated camera store with viewport camera management

--- src/events.d.ts
+++ src/events.d.ts
@@ -1,7 +1,11 @@
 import type { ShadingMode } from './store/types/shading'
+import type THREE from './three'
 
 declare global {
 	interface DocumentEventMap {
 		'shading:modeChange': CustomEvent<ShadingMode>
+		'scene:objectDeleted': CustomEvent<THREE.Object3D>
+		'scene:lightAdded': CustomEvent<THREE.Object3D>
+		'scene:modelAdded': CustomEvent<THREE.Object3D>
 	}
 }

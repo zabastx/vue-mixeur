48551bb44d09f98932764f1b60bfe0c802982468
Danil Popov <zabastx@gmail.com>
2026-04-23 21:25:21 +0300

    fix(ui): fix color input transparency display

--- src/components/base/input/InputColor.vue
+++ src/components/base/input/InputColor.vue
@@ -4,7 +4,7 @@
 			<div
 				class="inline-grid rounded grid-cols-2 grid-rows-1 w-full checkerboard h-full
 					overflow-hidden"
-				:style="{ '--color-value': modelHex, '--alpha': modelAlpha }"
+				:style="{ '--color-value': displayColor, '--alpha': modelAlpha }"
 			>
 				<div class="bg-(--color-value)"></div>
 				<div class="bg-(--color-value) opacity-(--alpha)"></div>
@@ -17,10 +17,20 @@
 </template>
 
 <script lang="ts" setup>
-defineProps<{
+import { computed } from 'vue'
+
+const { transparency } = defineProps<{
 	transparency?: boolean
 }>()
 
 const modelHex = defineModel<string>('hex', { required: true })
 const modelAlpha = defineModel<number>('alpha')
+
+const displayColor = computed(() => {
+	if (!transparency) return modelHex.value
+	if (modelHex.value.length === 9) {
+		return modelHex.value.slice(0, 7)
+	}
+	return modelHex.value
+})
 </script>

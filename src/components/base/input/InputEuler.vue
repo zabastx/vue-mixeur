a72457b421e36455c679c05fc47d57d76fd970f7
Danil Popov <zabastx@gmail.com>
2026-04-27 23:35:11 +0300

    feat(ui): add InputEuler and InputVector3 components

---
+++ src/components/base/input/InputEuler.vue
@@ -0,0 +1,65 @@
+<template>
+	<div>
+		<InputField label="X">
+			<InputNumber v-model="x" :min :max :step :format-options />
+		</InputField>
+		<InputField label="Y">
+			<InputNumber v-model="y" :min :max :step :format-options />
+		</InputField>
+		<InputField label="Z">
+			<InputNumber v-model="z" :min :max :step :format-options />
+		</InputField>
+	</div>
+</template>
+
+<script lang="ts" setup>
+import type THREE from '@/three'
+import { MathUtils } from 'three'
+import { computed, triggerRef } from 'vue'
+
+defineProps<{
+	min?: number
+	max?: number
+	step?: number
+}>()
+
+const model = defineModel<THREE.Euler>({
+	required: true
+})
+
+const x = computed<number>({
+	set(val) {
+		model.value.x = MathUtils.degToRad(val)
+		triggerRef(model)
+	},
+	get() {
+		return MathUtils.radToDeg(model.value.x)
+	}
+})
+
+const y = computed<number>({
+	set(val) {
+		model.value.y = MathUtils.degToRad(val)
+		triggerRef(model)
+	},
+	get() {
+		return MathUtils.radToDeg(model.value.y)
+	}
+})
+
+const z = computed<number>({
+	set(val) {
+		model.value.z = MathUtils.degToRad(val)
+		triggerRef(model)
+	},
+	get() {
+		return MathUtils.radToDeg(model.value.z)
+	}
+})
+
+const formatOptions: Intl.NumberFormatOptions = {
+	unit: 'degree',
+	unitDisplay: 'narrow',
+	style: 'unit'
+}
+</script>

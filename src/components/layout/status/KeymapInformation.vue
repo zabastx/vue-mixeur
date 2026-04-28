339f9eba4faa7dec1e4cbd0d560c4c20ae371bf2
Danil Popov <zabastx@gmail.com>
2026-04-28 23:41:11 +0300

    feat(keymap): show context-sensitive transform controls hints

--- src/components/layout/status/KeymapInformation.vue
+++ src/components/layout/status/KeymapInformation.vue
@@ -1,8 +1,13 @@
 <template>
-	<div v-show="appStore.pointerOnCanvas" class="flex items-center gap-1 text-xl select-none">
-		<div v-for="item in activeHints" :key="item.text" class="align-center flex">
-			<MxIcon :name="item.icon" />
-			<span class="text-xs">{{ item.text }}</span>
+	<div v-show="appStore.pointerOnCanvas" class="flex items-center gap-2 text-xs select-none">
+		<div v-for="item in activeHints" :key="item.text" class="align-center flex gap-1">
+			<MxIcon v-if="item.icon" class="text-xl" :name="item.icon" />
+			<div v-if="item.textHint" class="space-x-0.5">
+				<span v-for="hint in item.textHint" :key="item.key + hint" class="border rounded px-1">
+					{{ hint }}
+				</span>
+			</div>
+			<span>{{ item.text }}</span>
 		</div>
 	</div>
 </template>
@@ -10,18 +15,45 @@
 <script lang="ts" setup>
 import { computed } from 'vue'
 import { useAppStore } from '@/store/app'
+import { useControlsStore } from '@/store/controls'
 
 const appStore = useAppStore()
+const controlsStore = useControlsStore()
 
 const keymapList: KeymapOption[] = [
 	{ icon: 'input/lmb', text: 'Select', key: null },
 	{ icon: 'input/mmb', text: 'Rotate View', key: null },
-	{ icon: 'input/lmb', text: 'Select Toggle', key: 'shift' },
 	{ icon: 'input/mmb', text: 'Screen Space Pan View', key: 'shift' },
 	{ icon: 'input/mmb', text: 'Pan View', key: 'ctrl' }
 ]
 
+const transfromControlsHints: KeymapOption[] = [
+	{
+		textHint: ['X', 'Y', 'Z'],
+		key: null,
+		text: 'Axis'
+	},
+	{
+		textHint: ['C'],
+		key: null,
+		text: 'Clear Constraints'
+	},
+	{
+		textHint: ['Ctrl'],
+		key: null,
+		text: 'Snap'
+	},
+	{
+		textHint: ['ESC'],
+		key: null,
+		text: 'Cancel'
+	}
+]
+
 const activeHints = computed(() => {
+	if (controlsStore.isTransformDrag) {
+		return transfromControlsHints
+	}
 	if (appStore.isCtrlDown) {
 		return keymapList.filter((item) => item.key === 'ctrl')
 	}
@@ -32,7 +64,8 @@ const activeHints = computed(() => {
 })
 
 interface KeymapOption {
-	icon: MxIconName
+	icon?: MxIconName
+	textHint?: string[]
 	text: string
 	key: 'ctrl' | 'shift' | null
 }
